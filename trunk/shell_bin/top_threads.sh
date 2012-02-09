96d9bb7250be5448fb19575fc093ba69440044e3
claudio@claudius.com.br <claudio@claudius.com.br@f662013e-e00c-11dd-ab27-75d9513d8d6c>
2012-02-09 16:01:15 +0000

    update

--- trunk/shell_bin/top_threads.sh
+++ trunk/shell_bin/top_threads.sh
@@ -2,12 +2,23 @@
 
 PID=$1
 IFS=''
+top_number=10
+if [ $# -gt 1 ] ; then
+  top_number=$2
+fi
+
+top_number=$((top_number+1))
 
 java_stack=`jstack -l $PID` 
-ps  -p $PID -o pid,lwp,%cpu,s,rss,etime  | head -1
-ps  -L h -p $PID -o pid,lwp,%cpu,s,rss,etime  | sort -r -n -k 3 | head -5
-ps  -L h -p $PID -o pid,lwp,%cpu,s,rss,etime  | sort -r -n -k 3 | head -5 | while read psline; do 
-  lwp_id=`echo $psline | awk '{print $2}'` 
+# mostrar apenas o cabecalho
+top=`top -s -b -H -p $PID -n 1 | grep -vE '^top|^Tasks|^Cpu|^Mem|^Swap' | grep -v '^$' | awk 'NR==1; NR > 1 {print $0 | "sort  -nrk 9"}' | head -$top_number`
+echo $top
+
+echo $top | while read psline; do 
+  if [ `echo $psline|grep -c PID` -gt 0 ] ;  
+    then continue
+  fi
+  lwp_id=`echo $psline | awk '{print $1}'` 
   nid=`printf '%x\n' $lwp_id ` 
   echo "========> Java LWP: $lwp_id - Native Thread ID=$nid"
   echo $java_stack | sed -n "/$nid/,/^$/ p" ;   
