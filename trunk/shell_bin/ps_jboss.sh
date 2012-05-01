97c08c02d106dae9aba79d1a125ca6d4d336b38f
claudio@claudius.com.br <claudio@claudius.com.br@f662013e-e00c-11dd-ab27-75d9513d8d6c>
2012-05-01 18:49:29 +0000

    melhorias

--- trunk/shell_bin/ps_jboss.sh
+++ trunk/shell_bin/ps_jboss.sh
@@ -6,10 +6,13 @@ ps -ef  | grep org.jboss.Main|grep -v grep |
 while read psline; do 
   pid=`echo $psline | awk '{print $2}'` 
   prc_name="EAP 5 - "`echo $psline | awk  -F ' -c ' '{ print $2}' `
-  info_prc=`ps -o nlwp,etime h $pid`
+  info_prc=`ps -o nlwp,etime,rss h $pid`
   nlwp=`echo $info_prc|awk '{print $1}'`
   etime=`echo $info_prc|awk '{print $2}'`
-  echo "$pid - $prc_name ($nlwp threads, since $etime)"
+  rss=`echo $info_prc|awk '{print $3}'`
+  echo "$pid - $prc_name ($nlwp threads, elapsed time $etime, RSS memory $(($rss/1024)) MB)"
+  jinfo $pid 2>&1|grep -E 'jboss.server.log.dir|jboss.home.dir'| sort|awk '{print "\t"$3}'
+  echo ""
   sudo netstat -antpuew | grep -E "$pid/java" | grep LISTEN | awk '{print "\t"$1" "$4}'
   echo ""
 done  
@@ -19,12 +22,18 @@ done
 ps -ef  | grep jboss-modules| grep '\-D\[' | 
 while read psline; do 
   pid=`echo $psline | awk '{print $2}'` 
-  prc_name=`echo $psline | awk  -F '-D' '{ print $2}' | awk -F ']' '{print $1"]"}' | sed -e 's/\[//g' -e 's/\]//g'`
+  prc_name="EAP 6/AS 7 "`echo $psline | awk  -F '-D' '{ print $2}' | awk -F ']' '{print $1"]"}' | sed -e 's/\[//g' -e 's/\]//g'`
   conf_name=`echo $psline | awk -F '-c ' '{print $2}'  ` 
   if [ "x" != "x"$conf_name ] ; then
     prc_name=$prc_name" ["$conf_name"]"
   fi
-  echo $pid" - "$prc_name
+  info_prc=`ps -o nlwp,etime,rss h $pid`
+  nlwp=`echo $info_prc|awk '{print $1}'`
+  etime=`echo $info_prc|awk '{print $2}'`
+  rss=`echo $info_prc|awk '{print $3}'`
+  echo "$pid - $prc_name ($nlwp threads, elapsed time $etime, RSS memory $(($rss/1024)) MB)"
+  jinfo $pid 2>&1|grep -E 'jboss.server.log.dir|jboss.server.base.dir'| sort|awk '{print "\t"$3}'
+  echo ""
   sudo netstat -antpuew | grep -E "$pid/java" | grep LISTEN | awk '{print "\t"$1" "$4}'
   echo ""
 done
